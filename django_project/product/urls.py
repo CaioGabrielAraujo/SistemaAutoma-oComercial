f7bc8bbb103eddfdf3c81e06e45116997d80b1ac
CaioGabrielAraujo <caiohgabriel@gmail.com>
2017-06-07 11:39:15 -0300

    deleting product feature

--- django_project/product/urls.py
+++ django_project/product/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^cadastrar/', views.cadastrar, name="cadastrarProduto"),
     url(r'^list_products/$', views.list_products, name="listproducts"),
     url(r'^create_category/', views.create_category, name="create_category"),
-
-
+    url(r'^edit_products/(?P<product_id>[0-9]+)/', views.edit_product, name="edit_product"),
+    url(r'^delete_products/(?P<product_id>[0-9]+)/', views.delete_products, name="delete_products"),
 ]
--- django_project/product/views.py
+++ django_project/product/views.py
@@ -39,8 +39,27 @@ def cadastrar(request):
 
 
 
+@staff_member_required
+def edit_product(request,product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == "POST":
+        form = Create_Product_Form(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('products:list_products'))
+        return render(request, 'edit_products.html', {'form': form})
+
+    else:
+        form = Create_Product_Form(instance=product)
+        return render(request, 'edit_products.html', {'form': form})
+
 def list_products(request):
     context = {
         'all_products': Product.objects.all(),
     }
     return render(request, 'list_products.html', context)
+
+@staff_member_required
+def delete_products(request, product_id):
+    Product.objects.get(id=product_id).delete()
+    return HttpResponseRedirect(reverse('product:listproducts'))
