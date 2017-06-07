4f8b3f6ceec10d6f1d8f6990ed57c8ffc0a4a931
CaioGabrielAraujo <caiohgabriel@gmail.com>
2017-06-07 10:24:44 -0300

    fixing create product

--- django_project/product/models.py
+++ django_project/product/models.py
@@ -4,17 +4,10 @@ from django.db import models
 
 # Create your models here.
 
-class Category(models.Model):
-    category_name = models.CharField(max_length=30, unique=True)
-
-    def __str__(self):
-        return self.category_name
-
 class Product(models.Model):
     nome = models.CharField(max_length=30)
     quantidade = models.IntegerField()
     precoUnidade = models.DecimalField(max_digits=8, decimal_places=2)
-    categories = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
-        return self.product_name
+        return self.nome
--- django_project/product/views.py
+++ django_project/product/views.py
@@ -2,22 +2,28 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from .forms import Create_Product_Form
-from .models import Product, Category
+
+from .models import Product
+from django.db import models
 
 
 
 def cadastrar(request):
-    if request.method == "POST":
-        form = Create_Product_Form(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('index'))
-        return render(request, 'cadastrarProduto.html', {'form': form})
-
+    if request.method == "GET":
+        return render(request, 'cadastrarProduto.html')
     else:
-        form = Create_Product_Form()
-        return render(request, 'cadastrarProduto.html', {'form': form})
+        form = request.POST
+        nome = form.get('Nomedoproduto')
+        quantidade = form.get('Quantidade')
+        preco = float(form.get('Preco'))
+
+
+
+        product = Product(nome=nome, quantidade=quantidade, precoUnidade=preco)
+        product.save()
+
+
+    return render(request, 'index.html')
 
 
 def list_products(request):
@@ -25,19 +31,3 @@ def list_products(request):
         'all_products': Product.objects.all(),
     }
     return render(request, 'list_products.html', context)
-
-def create_category(request):
-
-    if request.method == "POST":
-        form = Create_Category_Form(request.POST)
-        if form.is_valid():
-            form.save()
-        return render(request, 'createCategory/create_category.html', {'form': form, 'all_categories': Category.objects.all()})
-
-    else:
-        form = Create_Category_Form()
-        return render(request, 'createCategory/create_category.html', {'form': form, 'all_categories': Category.objects.all()})
-
-def delete_categories(request, category_id):
-        Category.objects.get(id=category_id).delete()
-        return HttpResponseRedirect(reverse('products:create_category'))
