63292303e8ef115070f5ef9543ea2ad6c77e22f0
CaioGabrielAraujo <caiohgabriel@gmail.com>
2018-03-20 19:58:58 -0300

    testing

--- django_project/users/urls.py
+++ django_project/users/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 app_name = 'users'
 urlpatterns = [
-    url(r'^dashboard/$', views.dashboard, name="dashboard"),
+    url(r'^dash/$', views.dashboard, name="dashboard"),
     url(r'^login/$', views.show_login, name="login"),
     url(r'^logout/$', views.logout, name="logout"),
     url(r'^self_edit/', views.self_edit_user, name="self_edit_user"),
